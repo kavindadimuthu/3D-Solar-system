74b372871588d40f3d73ca2b910fa733cf855d3a
kavindadimuthu <kavindadimuthu260@gmail.com>
2024-12-25 21:56:56 +0530

    orbit paths added

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'dat.gui';
 
 import starsTexture from './src/img/stars.jpg';
 import sunTexture from './src/img/sun.jpg';
@@ -39,6 +40,89 @@ orbit.update();
 
 const ambientLight = new THREE.AmbientLight(0xFFFFFF, 1);
 scene.add(ambientLight);
+
+
+// Function to create an orbit path
+function createOrbitPath(radius, color = 0xffffff, thickness = 1, opacity = 1) {
+    const curve = new THREE.EllipseCurve(
+        0, 0,            // ax, aY
+        radius, radius,  // xRadius, yRadius
+        0, 2 * Math.PI,  // aStartAngle, aEndAngle
+        false,           // aClockwise
+        0                // aRotation
+    );
+
+    const points = curve.getPoints(100);
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+    const material = new THREE.LineBasicMaterial({ 
+        color: color,
+        linewidth: thickness,
+        opacity: opacity,
+        transparent: opacity < 1
+    });
+
+    const ellipse = new THREE.Line(geometry, material);
+    ellipse.rotation.x = Math.PI / 2; // Rotate the ellipse to lie flat
+
+    return ellipse;
+}
+
+// Add orbit paths for each planet with custom properties
+const mercuryOrbit = createOrbitPath(28, 0xff0000, 2, 0.3);
+scene.add(mercuryOrbit);
+
+const venusOrbit = createOrbitPath(44, 0x00ff00, 2, 0.3);
+scene.add(venusOrbit);
+
+const earthOrbit = createOrbitPath(62, 0x0000ff, 2, 0.3);
+scene.add(earthOrbit);
+
+const marsOrbit = createOrbitPath(78, 0xffff00, 2, 0.3);
+scene.add(marsOrbit);
+
+const jupiterOrbit = createOrbitPath(100, 0xff00ff, 2, 0.3);
+scene.add(jupiterOrbit);
+
+const saturnOrbit = createOrbitPath(138, 0x00ffff, 2, 0.3);
+scene.add(saturnOrbit);
+
+const uranusOrbit = createOrbitPath(176, 0xffffff, 2, 0.3);
+scene.add(uranusOrbit);
+
+const neptuneOrbit = createOrbitPath(200, 0xffa500, 2, 0.3);
+scene.add(neptuneOrbit);
+
+const plutoOrbit = createOrbitPath(216, 0x800080, 2, 0.3);
+scene.add(plutoOrbit);
+
+
+// Add dat.GUI to control visibility of orbits
+const gui = new dat.GUI();
+const orbitFolder = gui.addFolder('Orbits');
+
+const orbits = {
+    all: true
+};
+
+function updateAllOrbitsVisibility(value) {
+    mercuryOrbit.visible = value;
+    venusOrbit.visible = value;
+    earthOrbit.visible = value;
+    marsOrbit.visible = value;
+    jupiterOrbit.visible = value;
+    saturnOrbit.visible = value;
+    uranusOrbit.visible = value;
+    neptuneOrbit.visible = value;
+    plutoOrbit.visible = value;
+}
+
+orbitFolder.add(orbits, 'all').name('Show orbits').onChange(value => {
+    updateAllOrbitsVisibility(value);
+});
+
+orbitFolder.open();
+
 
 const cubeTextureLoader = new THREE.CubeTextureLoader();
 scene.background = cubeTextureLoader.load([
